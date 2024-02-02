33f73fba2c26e889add9ac8203114def34ef8518
QulamSamadli <qulam999@gmail.com>
2024-02-03 01:36:20 +0400

    Navbar finished codes

--- app/Components/Navbar/index.tsx
+++ app/Components/Navbar/index.tsx
@@ -1,12 +1,13 @@
 
 import Image from 'next/image'
-
 import Link from 'next/link'
 import React from 'react'
+import { SlBasket } from "react-icons/sl";
+import { FaAngleDown } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <header className=' container bg-[#1B3764] p-4  '>
+    <header className=' container flex justify-between bg-[#1B3764] p-4  '>
 <div className='flex gap-4  '>
    <Link href="/"><Image
    width={118}
@@ -39,14 +40,14 @@ const Navbar = () => {
    </div>
   
 </div>
-<div>
-   <Link href="">Home</Link>
-   <Link href="">Pages</Link>
-   <Link href="">About</Link>
-   <Link href="">Services</Link>
-   <Link href="">Contact</Link>
-   <Link href=""></Link>
-   <Link href=""></Link>
+<div className='flex items-center gap-4 text-white'>
+   <Link href="/">Home</Link>
+   <Link href="/pages" className='flex items-center'>Pages <FaAngleDown /></Link>
+   <Link href="/about">About</Link>
+   <Link href="/services">Services</Link>
+   <Link href="/contact">Contact</Link>
+   <Link href="/basket"><SlBasket /></Link>
+   <Link href="/order"><button className='bg-[#FFCA42] text-center p-2 text-[#1B3764] rounded-lg'>Order Today</button></Link>
     
 </div>
 
