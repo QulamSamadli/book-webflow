b6f866a8a54b03da9907264450b611dee6dbfbc2
QulamSamadli <qulam999@gmail.com>
2024-02-03 01:23:47 +0400

    added navbar

--- app/Components/Navbar/index.tsx
+++ app/Components/Navbar/index.tsx
@@ -6,43 +6,45 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='container '>
-<div>
+    <header className=' container bg-[#1B3764] p-4  '>
+<div className='flex gap-4  '>
    <Link href="/"><Image
    width={118}
    height={35}
    src="/Logo.png"
    alt='Logo'
    /></Link>
-   <Link href="https://www.facebook.com/" className='border-4 bg-[black]  '>
+   <div className="flex gap-4">
+   <Link href="https://www.facebook.com/" className='border '>
     <Image src="/Facebook.png"
     alt='Facebook'
-    width={19}
+    width={29}
     height={10}
     />
    </Link>
-   <Link href="https://twitter.com/" className='border-4 '>
+   <Link href="https://twitter.com/" className='border'>
     <Image src="/Twitter.png"
     alt='Facebook'
-    width={19}
+    width={29}
     height={10}
     />
    </Link>
-   <Link href="https://www.linkedin.com/" className='border-4 '>
+   <Link href="https://www.linkedin.com/" className='border'>
     <Image src="/LinkedIn.png"
     alt='Facebook'
-    width={19}
+    width={29}
     height={10}
     />
    </Link>
+   </div>
   
 </div>
 <div>
-   <Link href=""></Link>
-   <Link href=""></Link>
-   <Link href=""></Link>
-   <Link href=""></Link>
-   <Link href=""></Link>
+   <Link href="">Home</Link>
+   <Link href="">Pages</Link>
+   <Link href="">About</Link>
+   <Link href="">Services</Link>
+   <Link href="">Contact</Link>
    <Link href=""></Link>
    <Link href=""></Link>
     
