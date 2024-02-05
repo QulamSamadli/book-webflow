a615a7092dcbcc26167c907c80efdbdf94dc0999
QulamSamadli <qulam999@gmail.com>
2024-02-05 17:42:06 +0400

    chnages tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,30 +8,35 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
     },
-    screens: { 
-      'sm': '640px', 
+    screens: {
+      'sm': '640px',
       // => @media (min-width: 640px) { ... } 
- 
-      'md': '768px', 
+
+      'md': '768px',
       // => @media (min-width: 768px) { ... } 
- 
-      'lg': '1024px', 
+
+      'lg': '1024px',
       // => @media (min-width: 1024px) { ... } 
- 
-      'xl': '1280px', 
+
+      'xl': '1280px',
       // => @media (min-width: 1280px) { ... } 
- 
-      
-     
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+
+
     },
-    container: { 
-      center: true, 
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
     },
   },
   plugins: [],
