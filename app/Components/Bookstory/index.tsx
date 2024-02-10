b8836d2cf4be9bf4c6d1b006aa46742c76e7b706
QulamSamadli <qulam999@gmail.com>
2024-02-11 01:41:16 +0400

    finished aurthor book

--- app/Components/Bookstory/index.tsx
+++ app/Components/Bookstory/index.tsx
@@ -18,33 +18,43 @@ interface Book {
 export default async function Bookstory() {
   const book = await getBook();
   return (
-    <div className="container ">
-      <h2 className="text-[45px] font-bold text-[#1B3764] text-center">
+    <div className="container my-10">
+      <h2 className="text-[45px] my-10 font-bold text-[#1B3764] text-center">
         Author’s Book Includes
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid  grid-cols-1 md:grid-cols-2 ">
         {book.map(({ id, title, description, pages, length, img }: Book) => {
           return (
-            <div className="flex" key={id}>
-              <div>
-                <Image src={img} alt={title} width={300} height={400} />
-              </div>
+            <div className="flex gap-4" key={id}>
               <div className="">
-                <h2>{title}</h2>
-                <p>{description}</p>
-                <div>
-                  <div>
-                    <div></div>
-                    <p>Pages:</p>
-                    <p>{pages}</p>
+                <Image src={img} alt={title} width={400} height={500} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <h2 className="text-[32px] text-[#1B3764] font-bold">
+                  {title}
+                </h2>
+                <p className="text-[19px] text-[#969AA0]">{description}</p>
+                <div className="grid grid-cols-2">
+                  <div className="flex ">
+                    <div className="w-[16px] h-[16px] rounded-full m-3 bg-[#FFCA42]"></div>
+                    <div>
+                      <p className="text-[24px] text-[#1B3764] font-bold">
+                        Pages:
+                      </p>
+                      <p className="text-[18px] text-[#969AA0]"> {pages}</p>
+                    </div>
                   </div>
-                  <div>
-                    <div></div>
-                    <p>Length:</p>
-                    <p>{length}</p>
+                  <div className="flex">
+                    <div className="w-[16px] h-[16px] rounded-full m-3 bg-[#FFCA42]"></div>
+                    <div>
+                      <p className="text-[24px] text-[#1B3764] font-bold">
+                        Length:
+                      </p>
+                      <p className="text-[18px] text-[#969AA0]">{length}</p>
+                    </div>
                   </div>
                 </div>
-                <button className="text-center p-3 border border-[#FFCA42] transition duration-300 text-[#1B3764] ">
+                <button className="w-[200px] text-center p-3 border border-[#FFCA42] transition duration-300 text-[#1B3764] ">
                   Order Today
                 </button>
               </div>
